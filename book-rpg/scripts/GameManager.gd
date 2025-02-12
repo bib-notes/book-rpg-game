4875aa520fa6c13d72ffc4605365cdb73c3a168d
em-d3v <em_dev_9836@outlook.com>
2025-02-12 16:50:30 -0600

    added scripts

--- book-rpg/scripts/GameManager.gd
+++ book-rpg/scripts/GameManager.gd
@@ -1,1 +1,11 @@
 extends Node
+enum GameState{
+	PLAY,
+	PAUSE,
+	MENU,
+	
+}
+var maps: Array
+var current_map: String
+var current_state:GameState
+signal state_change(state:GameState)
---
+++ book-rpg/scripts/data/user_settings.gd
@@ -0,0 +1,7 @@
+@tool
+extends Resource
+@export_group("General")
+
+@export var bg_volume: int ## background music
+@export var sfx_volume: int ## sound effects volume
+@export_group("Player")
