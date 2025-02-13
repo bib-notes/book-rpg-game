b6fc055239b61ae5e9df8f714ec8cbc16bdea745
em-d3v <em_dev_9836@outlook.com>
2025-02-12 21:49:19 -0600

    added styles

---
+++ book-rpg/data/styles/paper_theme.gd
@@ -0,0 +1,8 @@
+extends Theme
+@export var text_color_1:Color = Color("#00303b")
+@export var text_color_2: Color = Color("#0d9068")
+@export var color_3:Color = Color("#b02a36")
+@export var paper_1:Color = Color("#ecddc0")
+@export var paper_2:Color = Color("#ceb97a")
+@export var outline_1:Color = Color("#b4a794")
+@export var outline_2:Color = Color("#958979")
